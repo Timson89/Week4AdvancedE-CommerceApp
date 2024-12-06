f3e16e8fcedeff26d54febad47c916841ed8a8b0
Timothy <timothyolofson@gmail.com>
2024-12-05 23:35:30 -0800

    First Advanced E-Commerce App Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Timothy Olofson
+// December 5th 2024
+// Frontend Simplified (FES)
+// Advanced E-Commerce App 
+
+
+function openMenu() {
+
+  document.body.classList.add('menu--open');
+}
+
+function closeMenu() {
+
+  document.body.classList.remove('menu--open');
+}
